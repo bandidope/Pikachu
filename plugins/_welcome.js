24abc6949227204fea65fcb8e8f9d436267a1355
YALLICO OWNER <yallico2024@gmail.com>
2025-10-16 08:15:38 -0500

    Update _welcome.js

--- plugins/_welcome.js
+++ plugins/_welcome.js
@@ -50,7 +50,7 @@ export async function before(m, { conn, groupMetadata}) {
     // Imagen personalizada para bienvenida
     if (m.messageStubType === WAMessageStubType.GROUP_PARTICIPANT_ADD) {
       imgBuffer = await fetch(
-        'https://canvas-8zhi.onrender.com/api/welcome?title=Bienvenido&desc=al%20grupo%20Sasuke%20Bot&background=https://qu.ax/gcBQF.jpg'
+        'https://canvas-8zhi.onrender.com/api/welcome?title=Bienvenido&desc=al%20grupo%20Sasuke%20Bot&background=https://raw.githubusercontent.com/The-King-Destroy/Adiciones/main/Contenido/1745522645448.jpeg'
 ).then(res => res.buffer());
 }
 
